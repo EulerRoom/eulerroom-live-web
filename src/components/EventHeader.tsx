f69aa65f2894173866e15c82eb0515e777cfb2c0
alex <alex@slab.org>
2025-11-02 20:49:02 +0000

    bump

--- src/components/EventHeader.tsx
+++ src/components/EventHeader.tsx
@@ -45,19 +45,84 @@ function EventHeader({
     <header className="App-header">
       {/* <img src={logo} className="App-logo" alt="logo" /> */}
       <h2>Live Code Stream for Palestine</h2>
-<div class="flagbackground">
-  <div class="flagtop"></div>
-  <div class="flagmiddle"></div>
-  <div class="flagtriangle"></div>
-</div>
+      <div class="flagbackground">
+        <div class="flagtop"></div>
+        <div class="flagmiddle"></div>
+        <div class="flagtriangle"></div>
+      </div>
 
-<hr />
-<p>Please note this is in test mode - signups will be deleted.</p>
+      <hr />
+      <p>Please note this is in test mode - signups will be deleted.</p>
 
-<hr />
-      <p>Register a performance slot below.<br />
-      All live coders welcome, especially if this will be your first public performance.<br />
-      Once you have registered, please check your email for further instructions.</p>
+      <hr />
+
+      <p>
+        According to e.g.{" "}
+        <a href="https://genocidescholars.org/wp-content/uploads/2025/08/IAGS-Resolution-on-Gaza-FINAL.pdf">
+          leading scholars
+        </a>
+        , <a href="https://pchrgaza.org/">Palestinian</a>,{" "}
+        <a href="https://www.btselem.org/publications/202507_our_genocide">
+          Israeli
+        </a>{" "}
+        and{" "}
+        <a href="https://www.amnesty.org/en/latest/news/2024/12/amnesty-international-concludes-israel-is-committing-genocide-against-palestinians-in-gaza/">
+          International
+        </a>{" "}
+        human rights organisations, and{" "}
+        <a href="https://www.ohchr.org/en/press-releases/2025/09/israel-has-committed-genocide-gaza-strip-un-commission-finds">
+          the UN
+        </a>
+        ,<br /> Israel is carrying out genocide through the systematic
+        destruction of the Palestinian people in the Gaza strip.
+      </p>
+      <p>
+        Together we can try to help by: <br />
+        <br />
+        <strong>
+          Join the Palestinian-lead{" "}
+          <a href="https://bdsmovement.net/campaigns#1">BDS movement.</a>
+        </strong>
+        <br />
+        E.g. boycott Google, Amazon, Booking.com, Airbnb, Expedia, Teva,
+        Chevron, Intel, Dell, Siemens, HP, Microsoft, Carrefour, <br />
+        AXA, Reebok, Disney+, Sodastream, Re/max. <br /> Switching to ethical
+        banking/pension can be the easiest and most effective individual action
+        against the arms trade and climate breakdown.
+      </p>
+      <p>
+        <strong>Write to your local/national government representative.</strong>
+      </p>
+      <p>
+        <strong>Donate!</strong>
+        <br /> e.g.{" "}
+        <a href="https://www.map.org.uk/">Medical Aid for Palestinians</a>,
+        <a href="https://eleelnaelak.org/">Ele Elna Enak </a>,
+        <a href="https://chuffed.org/project/113222-tent-campaign-the-sameer-project">
+          Sameer
+        </a>
+        ,{" "}
+        <a href="https://chuffed.org/project/115245-dahnoun-mutual-aid">
+          Dahnoun
+        </a>
+      </p>
+      <p>
+        <strong>Spread the word.</strong>
+        <br />
+        E.g. by sharing this stream in your community and/or contributing a
+        performance.
+      </p>
+      <hr />
+
+      <p>
+        Register a performance slot below.
+        <br />
+        All live coders welcome, especially if this will be your first public
+        performance.
+        <br />
+        Once you have registered, please check your email for further
+        instructions.
+      </p>
       <h4>
         {" "}
         {event &&
