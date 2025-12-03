860dffd92e548746179958c335b24f9f849a8f55
HighHarmonics <highharmonics@gmail.com>
2025-12-02 21:42:14 -0800

    Update EventHeader.tsx
    Changes for the From Scratch event in Dec 2025
    Removed the content for the Palestine live code event.

--- src/components/EventHeader.tsx
+++ src/components/EventHeader.tsx
@@ -44,7 +44,7 @@ function EventHeader({
   return (
     <>
       <header>
-        <h1>Live Code Stream for Palestine</h1>
+        <h1>15 years of From Scratch</h1>
         <div className="flagbackground">
           <div className="flagtop"></div>
           <div className="flagmiddle"></div>
@@ -57,70 +57,8 @@ function EventHeader({
 
       <section>
         <p>
-          According to e.g.{" "}
-          <a href="https://genocidescholars.org/wp-content/uploads/2025/08/IAGS-Resolution-on-Gaza-FINAL.pdf">
-            leading scholars
-          </a>
-          , <a href="https://pchrgaza.org/">Palestinian</a>,{" "}
-          <a href="https://www.btselem.org/publications/202507_our_genocide">
-            Israeli
-          </a>{" "}
-          and{" "}
-          <a href="https://www.amnesty.org/en/latest/news/2024/12/amnesty-international-concludes-israel-is-committing-genocide-against-palestinians-in-gaza/">
-            International
-          </a>{" "}
-          human rights organisations, and{" "}
-          <a href="https://www.ohchr.org/en/press-releases/2025/09/israel-has-committed-genocide-gaza-strip-un-commission-finds">
-            the UN
-          </a>
-          , Israel is carrying out genocide through the systematic destruction
-          of the Palestinian people in the Gaza strip.
-        </p>
-        <p style={{ paddingTop: "18px" }}>
-          Together we can take active steps to help:
-        </p>
-        <p>
-          <strong>
-            ➔ &nbsp;Join the Palestinian-led{" "}
-            <a href="https://bdsmovement.net/campaigns#1">BDS movement</a>.
-          </strong>
-          <br />
-          e.g. Boycott Google, Amazon, Booking.com, Airbnb, Expedia, Teva,
-          Chevron, Intel, Dell, Siemens, HP, Microsoft, Carrefour, AXA, Reebok,
-          Disney+, Sodastream, Re/max. Switching to ethical banking/pension can
-          be the easiest and most effective individual action against the arms
-          trade and climate breakdown.
-        </p>
-        <p>
-          <strong>
-            ➔ &nbsp;Write to your local/national government representative.
-          </strong>
-          <br />
-          Depending on where you live, you could raise the issues of e.g. arms
-          sales to Israel, humanitarian aid, campaigns to end Israels aid
-          blockades, and your government's obligations to take all available
-          steps to end genocide under international humanitarian law.
-        </p>
-        <p>
-          <strong>➔ &nbsp;Donate!</strong>
-          <br />
-          e.g. To{" "}
-          <a href="https://www.map.org.uk/">
-            Medical Aid for Palestinians
-          </a>, <a href="https://eleelnaelak.org/">Ele Elna Enak </a>,{" "}
-          <a href="https://chuffed.org/project/113222-tent-campaign-the-sameer-project">
-            Sameer
-          </a>
-          ,{" "}
-          <a href="https://chuffed.org/project/115245-dahnoun-mutual-aid">
-            Dahnoun
-          </a>
-        </p>
-        <p>
-          <strong>➔ &nbsp;Spread the word.</strong>
-          <br />
-          e.g. By sharing this stream in your community and/or contributing a
-          performance.
+        From Scratch is an event organized by Toplap BCN (Barcelona).
+          Sessions should be done with code from scratch."
         </p>
       </section>
       <hr />
@@ -160,8 +98,7 @@ function EventHeader({
         <p>
           Register for a performance slot below.
           <br />
-          All live coders are welcome, especially if this will be your first
-          public performance.
+          All live coders are welcome.
           <br />
           Once you've registered, please check your email for further
           instructions.
