cbe752be7d95d3b491fecbdbd9538e331b6f2860
Alex McLean <alex@slab.org>
2025-12-04 13:40:15 +0000

    Set back to 15 mins because lina says so

--- src/components/PerformanceList.tsx
+++ src/components/PerformanceList.tsx
@@ -13,7 +13,7 @@ interface Props {
   setTotalStreamCount: (ttalStreamCount: number | null) => void;
 }
 
-const SLOT_DURATION_MIN = 12;
+const SLOT_DURATION_MIN = 15;
 
 const PerformanceList = ({
   slug,
